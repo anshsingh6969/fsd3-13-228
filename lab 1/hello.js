80d8ff85fccad6bf88c988eca700f73736559a68
anshsingh6969 <anshsingh01102006@gmail.com>
2026-07-21 14:49:34 +0530

    done for today

---
+++ lab 1/hello.js
@@ -0,0 +1,17 @@
+function sayHello(name) {
+console.log(`welcome to ${name}!`);
+}
+const Hello = (name) => {
+    console.log(`welcome to ${name}!`);
+}
+function sum(a,b) {
+
+    return a+b;
+}
+const findSum = (a,b) => {
+    return a+b;
+}
+sayHello("sallu bhai");
+console.log(sum(2,3));
+Hello("danish bhai");
+console.log(findSum(2,6));
